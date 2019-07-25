d9d5e07e91b34768bfef917864446c026bbc0b09
“Youryuconsecration <1077767895@qq.com>
2019-07-25 09:00:25 +0800

    标签的封装

---
+++ Tags.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tags{
+    public const string player = "Player";//代表玩家
+    public const string gameController = "GameController";//代表游戏控制器
+    public const string enemy = "Enemy";//代表敌人
+    public const string fader = "Fader";//代表淡入淡出的画布
+    public const string mainCamera = "MainCamera";//代表主摄像机
+}
+
