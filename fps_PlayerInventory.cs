ff65ca0841645a36b656e0978c88626ddfc886bf
“Youryuconsecration <1077767895@qq.com>
2019-07-29 10:24:09 +0800

    钥匙卡设定

---
+++ fps_PlayerInventory.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fps_PlayerInventory : MonoBehaviour
+{
+    //设定集合进行钥匙卡ID的存储
+    private List<int> keysArr;
+    private void Start()
+    {
+        keysArr = new List<int>();
+    }
+    //添加钥匙卡
+    public void AddKey(int keyId)
+    {
+        if (!keysArr.Contains(keyId))
+            keysArr.Add(keyId);
+    }
+    //判断用户是否拥有钥匙卡
+    public bool HasKey(int doorId)
+    {
+        if (keysArr.Contains(doorId))
+        {
+            return true;
+        }
+        else
+            return false;
+    }
+}
